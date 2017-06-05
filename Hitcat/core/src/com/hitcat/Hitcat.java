1416ed532da507227dcf4900d47db45909445814
Zbigniew Tomanek <zbiginewt@gmail.com>
2017-06-05 09:13:26 +0200

    wstępny szkielet

--- Hitcat/core/src/com/hitcat/Hitcat.java
+++ Hitcat/core/src/com/hitcat/Hitcat.java
@@ -7,28 +7,26 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Hitcat extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+	public SpriteBatch batch;
+	public static final int V_WIDTH = 800;
+	public static final int V_HEIGHT = 600;
+	public static float PPM = 100;
+	
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		setScreen(new GameScreen(this));
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 1, 1, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.draw(img, 300, 0);
-		batch.end();
+		super.render();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+
 	}
 }
---
+++ Hitcat/core/src/screens/GameScreen.java
@@ -0,0 +1,58 @@
+package screens;
+
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.hitcat.Hitcat;
+
+public class GameScreen implements Screen {
+	private Hitcat game;
+	private TiledMap map;
+	
+	
+	public GameScreen(Hitcat game){
+		this.game = game;
+	}
+
+	@Override
+	public void show() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void render(float delta) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void hide() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
