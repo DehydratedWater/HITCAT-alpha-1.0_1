7973a360006fbca1e1f4490de0f6c256a1deae65
DehydratedWater <ignacy.daszkiewicz@gmail.com>
2017-06-05 09:29:31 +0200

    Test push

--- Hitcat/core/src/screens/GameScreen.java
+++ Hitcat/core/src/screens/GameScreen.java
@@ -1,33 +1,53 @@
 package screens;
 
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.Camera;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.hitcat.GameConstants;
 import com.hitcat.Hitcat;
 
-public class GameScreen implements Screen {
+public class GameScreen implements Screen, GameConstants{
+	
 	private Hitcat game;
 	private TiledMap map;
+	private AssetManager assetManager;
+	private Camera cam;
+	private World world;
+	private Box2DDebugRenderer b2rend;
+	private FitViewport viewPort;
 	
 	
 	public GameScreen(Hitcat game){
 		this.game = game;
+		this.assetManager = game.assManager;
+		
+		cam = new OrthographicCamera();
+		viewPort = new FitViewport(V_WIDTH, V_HEIGHT, cam);
 	}
 
 	@Override
-	public void show() {
-		// TODO Auto-generated method stub
+	public void show() 
+	{
+		
 		
 	}
 
 	@Override
-	public void render(float delta) {
-		// TODO Auto-generated method stub
+	public void render(float delta) 
+	{
+		
 		
 	}
 
 	@Override
-	public void resize(int width, int height) {
-		// TODO Auto-generated method stub
+	public void resize(int width, int height) 
+	{
+		
 		
 	}
 
